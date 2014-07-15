1b1546efee2f5ad4189374e1076f82acc75ee29d
root <root@localhost.localdomain>
2014-07-14 23:42:38 -0500

    Files used to add skills

---
+++ components/com_community/models/colcre.php
@@ -0,0 +1,32 @@
+<?php
+//redacron script Colcre.php
+('_JEXEC') or die('Restricted access');
+
+require_once( JPATH_ROOT .'/components/com_community/models/models.php' );
+
+class CommunityModelColcre extends JCCModel
+{
+    function getSkillsList()
+    {
+        $db = JFactory::getDbo();
+        $query = "SELECT * FROM #__pf_skill_category ORDER BY category";
+        $db->setQuery($query);
+        $rows = $db->loadObjectList();
+        return $rows;
+    }
+    function getUserSkills($userid)
+    {
+        if (!is_numeric($userid))
+        {
+            return false;
+        }
+        else
+        {
+             $db = JFactory::getDbo();
+             $query = "SELECT b.* FROM #__pf_user_skills as a INNER JOIN #__pf_skills as b ON a.skill_id = b.id WHERE a.user_id = $userid ORDER BY b.skill";
+             $db->setQuery($query);
+             $rows = $db->loadObjectList();
+             return $rows;
+        }
+    }
+}
