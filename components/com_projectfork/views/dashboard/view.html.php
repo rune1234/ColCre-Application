2f68c452c03248a05164697078ac46e62241d1d2
root <root@localhost.localdomain>
2014-08-12 05:08:34 -0500

    Match functions

--- components/com_projectfork/views/dashboard/view.html.php
+++ components/com_projectfork/views/dashboard/view.html.php
@@ -22,10 +22,7 @@ class ProjectforkViewDashboard extends JViewLegacy
     protected $item;
     protected $pageclass_sfx;
     protected $toolbar;
-
-private function getCandidates($userid)
-{
-    $query = "SELECT users.name DeveloperName, 
+    protected $commonQuery = "SELECT users.name DeveloperName, users.id user_id,
 	projects.title ProjectTitle, 
 	project_skills.task_id + 1 MatchingTaskId, 
 	skills.skill SkillName, 
@@ -57,17 +54,116 @@ WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_i
 		FROM #__pf_user_skills user_skills_2
 		WHERE user_skills_2.user_id = users.id
 	)
-)
-AND users.id != $userid";
+)";
+
+private function getCandidates($userid)
+{
+    $query = "SELECT users.name DeveloperName, users.id user_id,
+	projects.title ProjectTitle, 
+	project_skills.task_id + 1 MatchingTaskId, 
+	skills.skill SkillName, 
+	round(1 / (
+		SELECT count(user_skills.user_id)
+		FROM #__pf_user_skills user_skills
+		WHERE user_skills.user_id = users.id
+	) * 100, 0) AS TaskMatchPercentage,
+	round(
+		1 / (
+			SELECT count(user_skills.user_id)
+			FROM #__pf_user_skills user_skills
+			WHERE user_skills.user_id = users.id
+		) * (1 / (
+			SELECT count(project_skills.task_id)
+			FROM #__pf_project_skills project_skills
+			WHERE project_skills.project_id = projects.id
+		)) * 100, 0
+	) AS ProjectMatchPercentage
+FROM #__users users, 	
+	#__pf_project_skills project_skills
+JOIN #__pf_projects AS projects ON projects.id = project_skills.project_id
+JOIN #__pf_skills AS skills ON skills.id = project_skills.skill_id
+WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_id) IN (
+	SELECT project_skills_2.project_id, project_skills_2.task_id, project_skills_2.skill_id
+	FROM #__pf_project_skills project_skills_2
+	WHERE project_skills_2.skill_id IN (
+		SELECT user_skills_2.skill_id
+		FROM #__pf_user_skills user_skills_2
+		WHERE user_skills_2.user_id = users.id
+	)
+)";//
+//AND users.id != $userid";
       $db = JFactory::getDbo();
       $db->setQuery($query);
       $rows = $db->loadObjectList();
       return $rows;
 }
+private function getProjectCandidates($userid, $projectId)
+{
+    //$projectId = isset($_GET['id']) ? $_GET['id'] : '';
+    if (!is_numeric($projectId)) return;
+    
+    $query = $this->commonQuery." AND projects.id = $projectId AND users.id != $userid";
+   // echo $query
+      $db = JFactory::getDbo();
+      $db->setQuery($query);
+      $rows = $db->loadObjectList();
+      return $rows;
+}
+private function getPrjTskCandidates($userid, $taskid)
+{
+    $projectId = isset($_GET['id']) ? $_GET['id'] : '';
+    if (!is_numeric($projectId)) return;
+    
+    $query = $this->commonQuery." AND projects.id = $projectId AND users.id != $userid";
+      $db = JFactory::getDbo();
+      $db->setQuery($query);
+      $rows = $db->loadObjectList();
+      return $rows;
+}
+private function getMatchDesc($userId, $taskid)
+{
+    $db = JFactory::getDbo();
+     if (!is_numeric($taskid) || !is_numeric($userId) ) return;
+    $query = "SELECT * FROM #__pf_project_skills_added WHERE userid = $userId LIMIT 1";
+    //echo $query;
+     $db->setQuery($query);
+     $rows = $db->loadObject();
+     //print_r($rows);
+     return ($rows) ? $rows : false;
+}
+public function specifyMatch($description, $taskId, $projectId, $userId, $matchid)
+{
+     $skill = $this->getMatchDesc($userId, $matchid);
+     $desc = $skill->skillDesc;
+     if (!$desc) return;
+     $query = "SELECT users.name DeveloperName, projects.title ProjectTitle, MATCH (project_tasks.description) "
+            . "AGAINST ('$desc' IN NATURAL LANGUAGE MODE) FROM #__users users, "
+            . "#__pf_project_skills_added user_skills_added, #__pf_tasks AS project_tasks "
+            . "JOIN #__pf_projects AS projects ON projects.id = project_tasks.project_id WHERE "
+            . "project_tasks.id = '$taskId' AND project_tasks.project_id = '$projectId' AND "
+            . "user_skills_added.userid = users.id AND users.id = '$userId'";
+     echo "<br /><p style='font-size: 10px;'>".$query."</p><br />";
+     $db = JFactory::getDbo();
+     $db->setQuery($query);
+     $rows = $db->loadObjectList();
+     return $rows;
+}
+   private function getProjectTasks($id)
+   {
+       if (!is_numeric($id) || $id == 0) return false;
+       $query = "SELECT * FROM #__pf_tasks WHERE project_id = $id";
+       $db = JFactory::getDbo();
+       $db->setQuery($query);
+       $rows = $db->loadObjectList();
+       return $rows;
+   }
 	function display($tpl = null)
 	{
 	    $this->state   = $this->get('State');
         $this->item    = $this->get('Item');
+        
+        $this->tasks = $this->getProjectTasks($this->item->id);
+         
         $this->params  = $this->state->params;
         $this->modules = JFactory::getDocument()->loadRenderer('modules');
         $this->toolbar = $this->getToolbar();
@@ -75,7 +171,7 @@ AND users.id != $userid";
          
         $myUser = JFactory::getUser();
         $this->user = $myUser;
-        if ($this->item->created_by && $this->user->id) { $this->matches = $this->getCandidates($this->user->id); }
+        if (1 ==1 || ($this->item->created_by && $this->user->id)) { $this->matches = $this->getProjectCandidates($this->user->id, $this->item->id); }
         else $this->matches = false;
         $dispatcher	   = JDispatcher::getInstance();
 
