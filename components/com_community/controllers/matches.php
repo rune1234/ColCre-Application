937b913adb08a2d47cfe66953411c47c636f6745
root <root@localhost.localdomain>
2014-08-16 06:32:38 -0500

    Get some matches for the user (improved)

--- components/com_community/controllers/matches.php
+++ components/com_community/controllers/matches.php
@@ -4,7 +4,7 @@ class CommunityMatchesController extends CommunityBaseController
     function display()
     { 
         $model	=  $this->getModel ( 'matches' );
-		$msg	=  $model->getMatches ();
+		$matches	=  $model->getMatches ();
 		$modMsg	= array ();
  
 		$view	=  $this->getView ( 'matches' );
@@ -14,26 +14,10 @@ class CommunityMatchesController extends CommunityBaseController
 		{
 			return $this->blockUnregister();
 		}
-
-		// Add small avatar to each image
-		if (! empty ( $msg ))
-		{
-			foreach ( $msg as $key => $val )
-			{
-				// based on the grouped message parent. check the unread message
-				// count for this user.
-				$filter ['parent'] = $val->parent;
-				$filter ['user_id'] = $my->id;
-				$unRead = $model->countUnRead ( $filter );
-				$msg [$key]->unRead = $unRead;
-			}
-		}
-		$data = new stdClass ( );
-		$data->msg = $msg;
-
-		$newFilter ['user_id'] = $my->id;
-		$data->inbox = $model->countUnRead ( $newFilter );
-		$data->pagination =  $model->getPagination ();
+                $data = new stdClass ( );
+		$data->matches = $matches;
+                $data->user = $my;
+		 $data->pagination =  $model->getPagination ();
 		echo $view->get ( 'matches', $data );
     }
 }
