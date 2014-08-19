f4e1b38d79c6d611a6f640cd31d1d3ccd36030d2
root <root@localhost.localdomain>
2014-08-19 06:31:50 -0500

    function specifyMatch uses Jonathans query

--- components/com_community/models/matches.php
+++ components/com_community/models/matches.php
@@ -20,7 +20,7 @@ implements CNotificationsInterface
 
 			$db = $this->getDBO();
 
-		        $sql = "SELECT a.*, b.*,d.skill, c.title,c.alias,c.description, c.project_brief, c.created_by, c.created, c.access, c.state FROM #__pf_project_skills as a INNER JOIN #__pf_user_skills as b ON b.skill_id = a.skill_id INNER JOIN #__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id  WHERE b.user_id=$my->id";
+		        $sql = "SELECT a.*, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state FROM #__pf_project_skills as a INNER JOIN #__pf_user_skills as b ON b.skill_id = a.skill_id INNER JOIN #__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id  WHERE b.user_id=$my->id";
 			$db->setQuery($sql);
 			$result = $db->loadObjectList();
                         $limit 		= $this->getState('limit');
@@ -29,12 +29,46 @@ implements CNotificationsInterface
 				$this->_pagination = new JPagination(count($result), $limitstart, $limit );
 				$result = array_slice($result, $limitstart, $limit);
 			}
-
+                        $i = 0;
+                        foreach($result as $rs)
+                        {
+                           $spec = $this->specifyMatch($rs->description, $rs->task_id, $rs->project_id, $my->id);
+                           //print_r($spec);
+                          $result[$i]->MatchAgainst = ($spec) ? $spec->MatchAgainst : 0;
+                          $i++;
+                        }
 			return $result;
 		}
 
 		return null;
-	}
+                
+   }
+   private function getMatchDesc($userId)
+{
+    $db = JFactory::getDbo();
+     if (!is_numeric($userId) ) return;
+    $query = "SELECT * FROM #__pf_project_skills_added WHERE userid = $userId LIMIT 1";
+     
+     $db->setQuery($query);
+     $rows = $db->loadObject();
+     //print_r($rows);
+     return ($rows) ? $rows : false;
+}
+   private function specifyMatch($description, $taskId, $projectId, $userId)
+{
+     $skill = $this->getMatchDesc($userId);
+     $desc = $skill->skillDesc;
+     if (!$desc) return;
+     $query = "SELECT projects.title ProjectTitle,
+MATCH (project_tasks.description) AGAINST ('$desc' IN NATURAL LANGUAGE MODE) as MatchAgainst
+FROM #__pf_tasks AS project_tasks JOIN #__pf_projects AS projects ON projects.id = project_tasks.project_id
+WHERE project_tasks.id = '$taskId' AND project_tasks.project_id = '$projectId' LIMIT 1";
+      
+     $db = JFactory::getDbo();
+     $db->setQuery($query);
+     $rows = $db->loadObject();
+     return $rows;
+}
     public function getTotalNotifications( $userId )
     {
          $db = $this->getDBO();
