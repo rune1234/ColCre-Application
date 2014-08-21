d41369d211ca6323463d871b24526201be8e0cc2
root <root@localhost.localdomain>
2014-08-21 06:26:11 -0500

    The query has been dramatically improved. It is more like Jonathans than mine.

--- components/com_community/models/matches.php
+++ components/com_community/models/matches.php
@@ -21,7 +21,40 @@ implements CNotificationsInterface
 			$db = $this->getDBO();
 
 		        $sql = "SELECT a.*, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state FROM #__pf_project_skills as a INNER JOIN #__pf_user_skills as b ON b.skill_id = a.skill_id INNER JOIN #__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id  WHERE b.user_id=$my->id ORDER BY created DESC";
-			$db->setQuery($sql);
+			$sql = "SELECT projects.alias,
+   project_skills.*, 
+   projects.description, projects.title,
+	skills.skill, 
+	round(1 / (
+		SELECT count(user_skills.user_id)
+		FROM kba07_pf_user_skills user_skills
+		WHERE user_skills.user_id = users.id
+	) * 100, 0) AS TaskMatchPercentage,
+	round(
+		1 / (
+			SELECT count(user_skills.user_id)
+			FROM kba07_pf_user_skills user_skills
+			WHERE user_skills.user_id = users.id
+		) * (1 / (
+			SELECT count(project_skills.task_id)
+			FROM kba07_pf_project_skills project_skills
+			WHERE project_skills.project_id = projects.id
+		)) * 100, 0
+	) AS ProjectMatchPercentage
+FROM kba07_users users, 	
+	kba07_pf_project_skills project_skills
+JOIN kba07_pf_projects AS projects ON projects.id = project_skills.project_id
+JOIN kba07_pf_skills AS skills ON skills.id = project_skills.skill_id
+WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_id) IN (
+	SELECT project_skills_2.project_id, project_skills_2.task_id, project_skills_2.skill_id
+	FROM kba07_pf_project_skills project_skills_2
+	WHERE project_skills_2.skill_id IN (
+		SELECT user_skills_2.skill_id
+		FROM kba07_pf_user_skills user_skills_2
+		WHERE user_skills_2.user_id = users.id
+	) AND users.id =$my->id
+)";
+                        $db->setQuery($sql);
 			$result = $db->loadObjectList();
                         $limit 		= $this->getState('limit');
 		        $limitstart	= $this->getState('limitstart');
@@ -33,13 +66,13 @@ implements CNotificationsInterface
                         foreach($result as $rs)
                         {
                            $spec = $this->specifyMatch($rs->description, $rs->task_id, $rs->project_id, $my->id);
-                           $matchPercent = $this->getProjPercen($my->id, $rs->project_id);
+                           /*$matchPercent = $this->getProjPercen($my->id, $rs->project_id);
                            //print_r($matchPercent);
                            if ($matchPercent)
                            {
                                $result[$i]->TaskMatchPercentage = $matchPercent->TaskMatchPercentage;
                                $result[$i]->ProjectMatchPercentage = $matchPercent->ProjectMatchPercentage;
-                           }
+                           }*/
                           $result[$i]->MatchAgainst = ($spec) ? $spec->MatchAgainst : 0;
                           $i++;
                         }
@@ -74,7 +107,7 @@ WHERE project_tasks.id = '$taskId' AND project_tasks.project_id = '$projectId' L
      $db->setQuery($query);
      $rows = $db->loadObject();
      return $rows;
-}
+}/*
 private function getProjPercen($userid, $projectid)
 {
     if (!is_numeric($userid) || !is_numeric($projectid)) return;
@@ -105,7 +138,7 @@ private function getProjPercen($userid, $projectid)
     $db->setQuery($query);
 	 $result = $db->loadObject();
          return $result;
-}
+}*/
     public function getTotalNotifications( $userId )
     {
          $db = $this->getDBO();
