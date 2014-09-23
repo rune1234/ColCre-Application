271ffec9d89aba7672e40c9d5e174332f8864515
root <root@localhost.localdomain>
2014-09-23 01:31:06 -0500

    Matches is being improved with a library for it.

--- libraries/projectfork/colcre/matches.php
+++ libraries/projectfork/colcre/matches.php
@@ -2,13 +2,18 @@
 defined('_JEXEC') or die();
 class projectMatches
 {
-       public function specifyMatch($description, $taskId, $projectId, $userId)
+      function getDBO()
+      {
+          return JFactory::getDbo();
+      }
+      
+      public function specifyMatch($description, $taskId, $projectId, $userId)
        {
-             $db = JFactory::getDbo();
+             $db = $this->getDBO();
              if (!is_numeric($taskId)) return false;
              if (!is_numeric($projectId)) return false;
              if (!is_numeric($userId)) return false;
-             $skill = $this->getMatchDesc($userId, $db);
+             $skill = $this->_getMatchDesc($userId, $db);
              $desc = $skill->skillDesc;
              if (!$desc) return;
              $query = "SELECT projects.title ProjectTitle,
@@ -17,14 +22,14 @@ class projectMatches
         WHERE project_tasks.id = '$taskId' AND project_tasks.project_id = '$projectId' LIMIT 1";
 
              $db->setQuery($query);
-             $rows = $db->loadObject();
+             $rows = $db->loadObjectList();
              return $rows;
      }
      public function getUserMatches($userid)
      {
-         $db = $this->getDBO();
          if (!is_numeric($userid)) return;
-         $query = "SELECT a.*, e.title as task_title, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state, round((  (select count(*) "
+         $db = $this->getDBO();
+         $query = "SELECT a.*, e.title as task_title, b.*,d.skill, c.catid, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state, round((  (select count(*) "
             . "FROM #__pf_user_skills ab join #__pf_project_skills bc ON ab.skill_id = bc.skill_id where ab.user_id = $userid and bc.task_id = e.id) / "
             . "(SELECT count(cd.task_id) FROM `#__pf_project_skills` as cd WHERE cd.task_id = e.id) * 100)) as TaskMatchPercentage, round((select count(*) "
             . "FROM #__pf_user_skills de join #__pf_project_skills ef ON de.skill_id = ef.skill_id where de.user_id = $userid and ef.project_id = c.id)  / "
@@ -33,10 +38,11 @@ class projectMatches
             . "#__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id INNER "
             . "JOIN #__pf_tasks as e ON e.id = a.task_id WHERE b.user_id= $userid ORDER BY created DESC LIMIT 50";
          $db->setQuery($query);
-         $rows = $db->loadObject();
+         $rows = $db->loadObjectList();
+         
          return $rows;
      }
-        private function getMatchDesc($userId, & $db)
+        private function _getMatchDesc($userId, & $db)
     {
         //$db = JFactory::getDbo();
          if (!is_numeric($userId) ) return;
