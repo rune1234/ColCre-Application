12531e7ce4e120a98a9917791d4f9b0dc679b567
root <root@localhost.localdomain>
2014-10-06 04:46:54 -0500

    Matches are now in another file. A lot of code has been removed from here.

--- components/com_community/models/matches.php
+++ components/com_community/models/matches.php
@@ -2,35 +2,36 @@
 defined('_JEXEC') or die('Restricted access');
 
 require_once( JPATH_ROOT .'/components/com_community/models/models.php' );
+require_once( JPATH_ROOT .'/libraries/projectfork/colcre/matches.php' );//libraries/projectfork/colcre/matches.php
+
 
 class CommunityModelMatches extends JCCModel
 implements CNotificationsInterface
 {
     var $_pagination = null;
-    
+    var $user;
     public function getMatches($_isread=true)
 	{
 	    jimport('joomla.html.pagination');
 		$my = CFactory::getUser();
-		 
+		$this->user = $my;
 
 		if (empty($this->_data))
 		{
 			$this->_data = array();
-
-			$db = $this->getDBO();
-
-		        //$sql = "SELECT a.*, e.title as task_title, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state FROM #__pf_project_skills as a INNER JOIN #__pf_user_skills as b ON b.skill_id = a.skill_id INNER JOIN #__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id INNER JOIN #__pf_tasks as e ON e.id = a.task_id WHERE b.user_id=$my->id ORDER BY created DESC";
-		$sql = "SELECT a.*, e.title as task_title, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state, round((  (select count(*) "
+                        $pm = new projectMatches();
+                        $result = $pm->getUserMatches($my->id);
+                  /*  
+		 $sql = "SELECT a.*, e.title as task_title, b.*,d.skill, c.title,c.alias,c.description, c.created_by, c.created, c.access, c.state, round((  (select count(*) "
             . "FROM #__pf_user_skills ab join #__pf_project_skills bc ON ab.skill_id = bc.skill_id where ab.user_id = $my->id and bc.task_id = e.id) / "
             . "(SELECT count(cd.task_id) FROM `#__pf_project_skills` as cd WHERE cd.task_id = e.id) * 100)) as TaskMatchPercentage, round((select count(*) "
             . "FROM #__pf_user_skills de join #__pf_project_skills ef ON de.skill_id = ef.skill_id where de.user_id = $my->id and ef.project_id = c.id)  / "
             . "(SELECT count(ef.task_id) FROM `#__pf_project_skills` as ef WHERE project_id = c.id) * 100) as ProjectMatchPercentage "
             . "FROM #__pf_project_skills as a INNER JOIN #__pf_user_skills as b ON b.skill_id = a.skill_id INNER JOIN "
             . "#__pf_projects as c ON c.id = a.project_id INNER JOIN #__pf_skills as d ON d.id = a.skill_id INNER "
-            . "JOIN #__pf_tasks as e ON e.id = a.task_id WHERE b.user_id= $my->id ORDER BY created DESC ";	
-               //echo $query;
-                        /*$sql = "SELECT projects.alias,
+            . "JOIN #__pf_tasks as e ON e.id = a.task_id WHERE b.user_id= $my->id ORDER BY created DESC LIMIT 50";	
+                
+                        $sql = "SELECT projects.alias,
    project_skills.*, 
    projects.description, projects.title, projects.created,
 	skills.skill FROM kba07_users users, 	
@@ -46,20 +47,22 @@ WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_i
 		WHERE user_skills_2.user_id = users.id
 	) AND users.id =$my->id".// AND users.id != projects.created_by
 ") ORDER BY projects.created DESC";*/
-                       
-                        $db->setQuery($sql);
-			$result = $db->loadObjectList();
+                        
+                        /*$db->setQuery($sql);
+			$result = $db->loadObjectList();*/
                         //print_r($result);
-                        $limit 		= $this->getState('limit');
-		        $limitstart	= $this->getState('limitstart');
-			if (empty($this->_pagination)) {
+                         $limit 		= $this->getState('limit');
+		         $limitstart	= $this->getState('limitstart');
+                         $limitstart = JRequest::getInt('limitstart', '0');
+                         $limit = JRequest::getVar( "limit", '10', 'get', 'int');
+                         if (empty($this->_pagination)) {
 				$this->_pagination = new JPagination(count($result), $limitstart, $limit );
 				$result = array_slice($result, $limitstart, $limit);
 			}
                         $i = 0;
                         foreach($result as $rs)
                         {
-                           $spec = $this->specifyMatch($rs->description, $rs->task_id, $rs->project_id, $my->id);
+                           $spec = $pm->specifyMatch($rs->description, $rs->task_id, $rs->project_id, $my->id);
                            //$result[$i]->ProjectMatchPercentage = $this->getProjPercen($my->id, $rs->project_id);
                            //$result[$i]->TaskMatchPercentage = $this->getTaskPercen($my->id, $rs->task_id);
                            //print_r($matchPercent);
@@ -68,8 +71,8 @@ WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_i
                                $result[$i]->TaskMatchPercentage = $matchPercent->TaskMatchPercentage;
                                $result[$i]->ProjectMatchPercentage = $matchPercent->ProjectMatchPercentage;
                            }*/
-                          $result[$i]->MatchAgainst = ($spec) ? $spec->MatchAgainst : 0;
-                          $i++;
+                            $result[$i]->MatchAgainst = ($spec) ? $spec->MatchAgainst : 0;
+                            $i++;
                         }
 			return $result;
 		}
@@ -77,6 +80,7 @@ WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_i
 		return null;
                 
    }
+   /*
    private function getMatchDesc($userId)
 {
     $db = JFactory::getDbo();
@@ -88,6 +92,7 @@ WHERE (project_skills.project_id, project_skills.task_id, project_skills.skill_i
      //print_r($rows);
      return ($rows) ? $rows : false;
 }
+/*
    private function specifyMatch($description, $taskId, $projectId, $userId)
 {
      $skill = $this->getMatchDesc($userId);
@@ -103,7 +108,7 @@ WHERE project_tasks.id = '$taskId' AND project_tasks.project_id = '$projectId' L
      $rows = $db->loadObject();
      return $rows;
 }
-
+*/
 private function getProjPercen($userid, $projectid)
 {
     if (!is_numeric($userid) || !is_numeric($projectid)) return;
@@ -113,7 +118,7 @@ private function getProjPercen($userid, $projectid)
     $db->setQuery($query);
 	 $result = $db->loadResult();
          return $result;
-}
+}/*
 private function getTaskPercen($userid, $taskid)
 {
      if (!is_numeric($userid) || !is_numeric($taskid)) return;
@@ -124,7 +129,7 @@ FROM kba07_pf_project_skills` WHERE task_id = $taskid";
     $db->setQuery($query);
 	 $result = $db->loadResult();
          return $result;
-}
+}*/
     public function getTotalNotifications( $userId )
     {
          $db = $this->getDBO();
