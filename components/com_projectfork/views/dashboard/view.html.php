f8788443409f9a108308650d4781269fbcf0905c
root <root@rpcarnell>
2015-01-27 04:01:46 -0500

    Working on the comment permissions and adding a proposal system as well.

--- components/com_projectfork/views/dashboard/view.html.php
+++ components/com_projectfork/views/dashboard/view.html.php
@@ -153,13 +153,18 @@ private function getMatchDesc($userId)
    {
 	    $this->state   = $this->get('State');
             $this->item    = $this->get('Item');
-        require_once( JPATH_ROOT .'/libraries/projectfork/colcre/matches.php' );
+        //require_once( JPATH_ROOT .'/libraries/projectfork/colcre/matches.php' );
+        jimport('projectfork.colcre.project');
+        jimport('projectfork.colcre.matches');
+        $pd = new projectData();
+        $owner = $pd->projectOwner();
+        $this->owner = $owner;
         $this->tasks = $this->getProjectTasks($this->item->id);
          
         $this->params  = $this->state->params;
         $this->modules = JFactory::getDocument()->loadRenderer('modules');
         $this->toolbar = $this->getToolbar();
-         
+        
          
         $myUser = JFactory::getUser();
         $this->user = $myUser;
@@ -182,7 +187,7 @@ private function getMatchDesc($userId)
 		}
 
         // Process the content plugins.
-        if (!empty($this->item)) {
+        if (!empty($this->item)) {  
             // Fake content item
             PFObjectHelper::toContentItem($this->item);
 
@@ -203,9 +208,9 @@ private function getMatchDesc($userId)
     		$results = $dispatcher->trigger('onContentAfterDisplay', array($context, &$this->item, &$this->params, 0));
     		$this->item->event->afterDisplayContent = trim(implode("\n", $results));
         }
-
+//print_r($this->item);
         // Prepare the document
-        $this->prepareDocument();
+        $this->prepareDocument($this->item);
 
         // Display
 		parent::display($tpl);
@@ -216,7 +221,7 @@ private function getMatchDesc($userId)
 	 * Prepares the document
      *
 	 */
-	protected function prepareDocument()
+	protected function prepareDocument($item)
 	{
 		$app	 = JFactory::getApplication();
 		$menu    = $app->getMenu()->getActive();
@@ -232,10 +237,37 @@ private function getMatchDesc($userId)
 			$this->params->def('page_heading', JText::_('COM_PROJECTFORK_DASHBOARD_TITLE'));
 		}
 
-
+               
+                $extraMeta = '';
         // Set the page title
-		$title = $this->params->get('page_title', '');
-
+		//$title = $this->params->get('page_title', '');
+                 $title = ($item->title) ? "Colcre Project: ".$item->title : false;
+                 if ($item->category_alias)
+                 {
+                     $title .= " - Category: ".$item->category_alias;
+                     $extraMeta .= " - Category: ".$item->category_alias;
+                 }
+                 $desc = ($item->text) ? substr(strip_tags($item->text), 0, 155)."..." : "Colcre Project";
+                 
+                 $keywords = preg_split("/[\s,\,\.,\',\;\!,\?]/", strip_tags(str_replace(array('&amp;', '&quot;'), '', $item->text)));
+                 $stopwords = array("a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount", "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as", "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the");
+                 
+                 $keywords = array_diff($keywords, $stopwords);
+                 $keywords_2 = array();
+                 $k = 0;
+                 foreach ($keywords as $keys) 
+                 { 
+                      if ($k > 50) continue;
+                      if ($keys) { if (!in_array($keys, $keywords_2) && strlen($keys) > 2) { $keywords_2[] = $keys;  } } 
+                      $k++;
+                 }
+                 $keywords = implode(',', $keywords_2);
+                 if ($item->category_alias)
+                 
+                 {
+                     $keywords .= ",".$item->category_alias;
+                 }
+                     
 		if (empty($title)) {
 			$title = $app->getCfg('sitename');
 		}
@@ -253,15 +285,10 @@ private function getMatchDesc($userId)
 			$this->document->setMetadata('robots', $this->params->get('robots'));
 		}
 
-        // Set page description
-        if($this->params->get('menu-meta_description')) {
-            $this->document->setDescription($desc);
-        }
-
+         
+$this->document->setDescription(str_replace('"', '\'', $desc));
         // Set page keywords
-        if($this->params->get('menu-meta_keywords')) {
             $this->document->setMetadata('keywords', $keywords);
-        }
 
 		// Add feed links
 		if ($this->params->get('show_feed_link', 1)) {
