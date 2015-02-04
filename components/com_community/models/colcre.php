e0fb012263dedca5ba71d5019611c7c449923421
root <root@rpcarnell>
2015-02-04 06:32:50 -0500

    User-added skills will now use categories.

--- components/com_community/models/colcre.php
+++ components/com_community/models/colcre.php
@@ -9,18 +9,27 @@ class CommunityModelColcre extends JCCModel
     function getSkillsList()
     {
         $db = JFactory::getDbo();
-        $query = "SELECT * FROM #__pf_skill_category ORDER BY category";
+       // $query = "SELECT * FROM #__pf_skill_category ORDER BY category";
+        $query = "SELECT id, LOWER(title) as category FROM #__categories WHERE extension='com_pfprojects' ORDER BY LOWER(title)";
         $db->setQuery($query);
         $rows = $db->loadObjectList();
         return $rows;
     }
-    function getUserSkills($userid)
+    function getUserSkills($userid, $catg = '')
     {
         if (!is_numeric($userid)) { return false; }
         else
         {
              $db = JFactory::getDbo();
-             $query = "SELECT b.* FROM #__pf_user_skills as a INNER JOIN #__pf_skills as b ON a.skill_id = b.id WHERE a.user_id = $userid ORDER BY b.skill";
+        if (is_numeric($catg) && $catg > 0)
+        {
+            $catg = " AND a.skillCatg = $catg";
+        }
+        else {
+            $catg = '';
+            echo "Warning on colcre.php, catg is not a number";
+        }
+             $query = "SELECT b.* FROM #__pf_user_skills as a INNER JOIN #__pf_skills as b ON a.skill_id = b.id WHERE a.user_id = $userid $catg ORDER BY b.skill";
              $db->setQuery($query);
              $rows = $db->loadObjectList();
              return ($rows) ? $rows : false;
