3af11df1ee4343cda04df0e70c4dd3cba0b2d6fb
root <root@rpcarnell>
2015-04-20 23:47:39 -0500

    Finish or Unfinish a task without editing it

--- components/com_pftasks/views/task/tmpl/default.php
+++ components/com_pftasks/views/task/tmpl/default.php
@@ -44,6 +44,9 @@ $authorized = PFtasksHelper::taskPermission($item->id, $user->id);//redacron
 <div ng-app="myLikes">
             <div ng-controller="taskLike"  data-ng-init="getLikes(<?php echo $item->id; ?>, <?php echo $user->id;?>)">
                 <div ng-click="like(<?php echo $user->id;?>, <?php echo $item->id; ?>)" class="likemain"><div class="likelayer"></div><br /><span>Like</span> | <span style="color: #000; font-size: 12px;"><span ng-bind="likes"></span> Likes</span></div>
+                <div style="float: left; margin-left: 150px; margin-top: -33px;"><a href="#comments" style="color: #a00;"><img src="<?php echo JURI::base()."images/comicon.jpg";?>" alt="comment icon" style="height: 45px;"/>Comments</a></div>
+                  
+            
                 <div style="clear: both"></div>
             </div>
         </div><br />
@@ -119,13 +122,46 @@ $authorized = PFtasksHelper::taskPermission($item->id, $user->id);//redacron
         		</dd>
             <?php endif; ?>
     	</dl>
+   
         <div class="clearfix"></div>
 	</div>
-
+ 
 	<hr />
-
-    <?php echo $item->event->afterDisplayContent;
+<?php if ($user->id == $this->item->created_by)  { ?>
+        
+  <div class="formelm control-group">
+    		<div class="control-label">
+                    <label id="jform_complete-lbl" for="jform_complete" class="hasTooltip" title="<strong>Complete</strong><br />Select whether this task is completed or not."><b>Mark Task as Completed:</b></label>    	    </div>
+    	    <div class="controls">
+    	    	 
+                <div style='padding: 10px; color: #fff; background: #bd362f;   -webkit-box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05);
+  -moz-box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05);
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05); margin-bottom: 5px; width: 20%;'><input type="radio" id="jform_complete0" name="jformcomplete" class='jformcomplete' value="0" <?php echo (! $item->complete) ? "checked=\"checked\"" : ''; ?>  />&nbsp;&nbsp;<span>Project is not Completed</span></div>
+                        
+                    
+                <div style='padding: 10px; color: #fff; background: #51a351;   -webkit-box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05);
+  -moz-box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05);
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.15), 0 1px 2px rgba(0, 0, 0, 0.05); width: 20%;'><input type="radio" id="jform_complete1" name="jformcomplete" class='jformcomplete' value="1" <?php echo ($item->complete) ? "checked=\"checked\"" : ''; ?> />&nbsp;&nbsp;<span>Project is Completed</span></div>
+                       	    
+            </div>
+    	</div><br />
+<?php } ?>
+    <?php  echo $item->event->afterDisplayContent;
     if ($authorized) { ?><p><a href='<?php echo JRoute::_("index.php?option=com_pftasks&id=".$item->id."&view=taskform&layout=edit");?>'>Edit Task</a></p><?php } 
     if ($projerized) { ?><p><a href='<?php echo JRoute::_("index.php?option=com_pftasks&view=taskform&layout=edit");?>'>Add another Task for <?php echo $item->project_title;?></a></p><?php } ?>
         
 </div>
+<script><!--
+   jQuery(document).ready(function () {  
+        jQuery('.jformcomplete').change(function () { 
+             var $fragment_refresh = {
+		url: '<?php echo JUri::base(); ?>',
+		type: 'POST',
+		data: { option: 'com_pftasks', task: 'finishtask', taskid: '<?php echo $item->id;?>', finish : jQuery('input:radio[name=jformcomplete]:checked').val() },
+		success: function( data ) { console.log(data); } };
+             jQuery.ajax( $fragment_refresh );
+        });
+       
+        
+    });
+--></script>
