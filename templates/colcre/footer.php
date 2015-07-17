a19637cba597f0f9876058053a0a089db8a22f0e
root <root@rpcarnell>
2015-07-17 04:03:36 -0500

    The footer has been fixed

---
+++ templates/colcre/footer.php
@@ -0,0 +1,52 @@
+<footer id="footer">
+					<!--<jdoc:include type="modules" name="position-14" />-->
+                                        <br />
+                                        <table width='90%' style='color: #fff; margin-left: 10%;' id='footer-links'>
+                                            <tr class='top-links'>
+                                                <td><a href='<?php //echo JRoute::_('index.php'); ?>'>MakeWhatever</a></td>
+                                                <td><a href='<?php //echo JRoute::_('index.php'); ?>'>About Us</a></td>
+                                                <td rowspan='7' style='padding-left: 70px;'><h1>MakeWhatever</h1></td>
+                                                <td><!--<a href='<?php //echo JRoute::_('index.php'); ?>'>Connect</a>--></td>
+                                                <td><a href='<?php echo JRoute::_('index.php'); ?>'>Support</a></td>
+                                            </tr>
+                                            <tr>
+                                                <td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                <td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>
+                                            </tr>
+                                            <tr>
+                                                 
+                                                  <td><a href='<?php echo JRoute::_('index.php?option=com_k2&view=item&layout=item&id=1&Itemid=142'); ?>'>How it Works</a></td>
+                                                 <td><a href='<?php echo JRoute::_('index.php'); ?>'>Press</a></td>
+                                            
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Company</a><?php } ?></td>
+                                                 <td><a href='<?php echo JRoute::_('index.php'); ?>'>Contact Us</a></td>
+                                             <!--  <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Help</a><?php } ?></td>-->
+                                            </tr>
+                                           <!-- <tr>
+                                           <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Help</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Profiles</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Our Team</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Integration</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Terms of Service</a><?php } ?></td>
+                                            </tr>-->
+                                             <tr>
+                                                  <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Profiles</a><?php } ?></td>
+                                                <!--<td><a href='<?php echo JRoute::_('index.php'); ?>'>How it Works</a></td>-->
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Help</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { echo "fuf"; ?><a href='<?php echo JRoute::_('index.php'); ?>'>Jobs</a><?php } ?></td>
+<td><a href='<?php echo JRoute::_('index.php'); ?>'>Terms of Service</a></td>                                               
+<!-- <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>API</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Security</a><?php } ?></td>-->
+                                            </tr>
+                                            <tr>
+                                                 <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Project</a><?php } ?></td>
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Crowdsourcing</a><?php } ?></td>
+                                                
+                                                <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Engineering</a><?php } ?></td>
+                                                 <td><?php if (1 == 2) { ?><a href='<?php echo JRoute::_('index.php'); ?>'>Contact Us</a><?php } ?></td>
+                                            </tr>
+                                        </table>
+                                        <div id='footerfollow'><a href='<?php echo JRoute::_('index.php'); ?>'>Follow us on Twitter and Facebook</a></div>
+				</footer><!-- end footer -->
